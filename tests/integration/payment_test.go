432f0f3e8c2b9410d33a6316938c036b09bae478
Ash Wu <hsatac@smartpay.co>
2022-05-30 17:48:26 +0800

    Add test for list payments
    Signed-off-by: Ash Wu <hsatac@smartpay.co>

--- tests/integration/payment_test.go
+++ tests/integration/payment_test.go
@@ -22,11 +22,11 @@ func (suite *IntegrationTestSuite) TestCreateAPayment() {
 
 	suite.Run("TestRetrieveAPayment", func() {
 		params := RetrieveAPaymentParams{}
-		retrieveAPaymentResult, err := suite.client.RetrieveAPaymentWithResponse(suite.ctx, paymentId, &params)
+		result, err := suite.client.RetrieveAPaymentWithResponse(suite.ctx, paymentId, &params)
 		suite.Nil(err)
-		suite.NotNil(retrieveAPaymentResult.Body)
-		suite.NotNil(retrieveAPaymentResult.JSON200)
-		payment, _ := ConvertToStruct[Payment](retrieveAPaymentResult.JSON200)
+		suite.NotNil(result.Body)
+		suite.NotNil(result.JSON200)
+		payment, _ := ConvertToStruct[Payment](result.JSON200)
 		suite.EqualValues(string(*payment.Id), paymentId)
 	})
 
@@ -34,11 +34,33 @@ func (suite *IntegrationTestSuite) TestCreateAPayment() {
 		params := RetrieveAPaymentParams{
 			Expand: Ptr(RetrieveAPaymentParamsExpand(ExpandAll)),
 		}
-		retrieveAPaymentResult, err := suite.client.RetrieveAPaymentWithResponse(suite.ctx, paymentId, &params)
+		result, err := suite.client.RetrieveAPaymentWithResponse(suite.ctx, paymentId, &params)
 		suite.Nil(err)
-		suite.NotNil(retrieveAPaymentResult.Body)
-		suite.NotNil(retrieveAPaymentResult.JSON200)
-		payment, _ := ConvertToStruct[PaymentExpanded](retrieveAPaymentResult.JSON200)
+		suite.NotNil(result.Body)
+		suite.NotNil(result.JSON200)
+		payment, _ := ConvertToStruct[PaymentExpanded](result.JSON200)
 		suite.EqualValues(string(*payment.Id), paymentId)
+	})
+
+	suite.Run("TestListAllPayments", func() {
+		params := ListAllPaymentsParams{}
+		result, err := suite.client.ListAllPaymentsWithResponse(suite.ctx, &params)
+		suite.Nil(err)
+		suite.NotNil(result.Body)
+		suite.NotNil(result.JSON200)
+		obj, _ := ConvertToStruct[Payment]((*result.JSON200.Data)[0])
+		suite.NotNil(obj.Id)
+	})
+
+	suite.Run("TestListAllPaymentsExpanded", func() {
+		params := ListAllPaymentsParams{
+			Expand: Ptr(ListAllPaymentsParamsExpand(ExpandAll)),
+		}
+		result, err := suite.client.ListAllPaymentsWithResponse(suite.ctx, &params)
+		suite.Nil(err)
+		suite.NotNil(result.Body)
+		suite.NotNil(result.JSON200)
+		obj, _ := ConvertToStruct[PaymentExpanded]((*result.JSON200.Data)[0])
+		suite.NotNil(obj.Id)
 	})
 }
